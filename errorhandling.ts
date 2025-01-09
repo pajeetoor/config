7f1331e754a55eefe9026ea7afe4f0a49c527125
paj33tooor <139661626+pajeetoor@users.noreply.github.com>
2025-01-08 23:38:24 -0500

    Create errorhandling.ts

---
+++ errorhandling.ts
@@ -0,0 +1,13 @@
+// API Error Response
+interface ErrorResponse {
+  success: false;
+  error: string;
+  code: number;
+}
+
+// Error Codes
+- 400: Bad Request
+- 401: Unauthorized
+- 403: Forbidden
+- 404: Not Found
+- 500: Internal Server Error
